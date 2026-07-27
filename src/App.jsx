2277d59d667f0f443dcca409b52e90d7ebae4f10
eleshinlomo <seun.olatunji2@gmail.com>
2026-07-27 01:14:29 -0400

    Update readme

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = ()=>{
       <span className="px-2">and</span>
       <span className="text-pink-500">Semilore Olatunji</span>
 
-      <h2 className="font-bold">Project Director</h2>
+      <h2 className="font-bold">Product Director</h2>
        <span className="text-red-700">Oluwakemi Olatunji</span>
 
       <h2 className="font-bold">Software Director</h2>
