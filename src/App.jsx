22d43639476c9b0f101432e69218ceedcbe82998
eleshinlomo <seun.olatunji2@gmail.com>
2026-08-01 13:00:02 -0400

    added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
+import { useState } from "react"
 import Remote from "./components/Remote"
 
 
 const App = ()=>{
 
-  return (
-    <div className="text-center">
-       <Remote />
-      
+  const [showRemote, setShowRemote] = useState(false)
+  const [isIpAddress, setIsIpAddress] = useState(false)
 
+   
+  const handleShowRemote = ()=>{
+      setShowRemote(true)
+  }
+
+  const handleHideButton = ()=>{
+     setShowRemote(false)
+  }
+
+  return (
+    <div className="text-center text-white">
+      <div className="flex gap-4 justify-center items-center">
+        <button 
+        onClick={handleShowRemote}
+        className="bg-blue-500 rounded-2xl px-4">
+          Show Remote
+        </button>
+
+        <button 
+        onClick={handleHideButton}
+        className="bg-blue-500 rounded-2xl px-4">
+          Hide Remote
+        </button>
+     </div>
+
+       {
+        showRemote && <Remote />
+        }
     </div>
   )
 }
