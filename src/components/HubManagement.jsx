5ee40df1f32f9b99e34bb0f158661d9f9d3efad8
desireolatunji <desire.olatunji123@gmail.com>
2026-08-01 13:48:22 +0100

    i added Hubmannegment.jsx

---
+++ src/components/HubManagement.jsx
@@ -0,0 +1,33 @@
+import "./HubManagement.css";
+export default function HubManagement() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h2>Register a New TV Room Configuration</h2>
+
+        <input
+          type="text"
+          placeholder="Room Label (e.g. Bed Room 2)"
+        />
+
+        <input
+          type="text"
+          placeholder="Television IP Address (e.g. 10.0.0.92)"
+        />
+
+        <button className="save-btn">
+          Save TV Room Parameters
+        </button>
+      </div>
+
+      <div className="card menu-card">
+        <h3>Hide Hub Management Settings</h3>
+      </div>
+
+      <div className="card menu-card">
+        <h3>⚙ Add / Manage Household TV Database</h3>
+      </div>
+    </div>
+
+  );
+}
